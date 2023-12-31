8cce609955b6241d69948f949168e5bfb062ba2c
Jom Winston Magbag <bjomwinston0107@gmail.com>
2024-01-01 00:17:31 +0800

    [FIX] responsive

--- client/src/work/WorkContainer.tsx
+++ client/src/work/WorkContainer.tsx
@@ -22,20 +22,20 @@ export const WorkContainer: FC<WorkContainerProps> = ({ image, status, title, gi
             <div className='flex flex-col  px-2'>
                 <p className='text-base  font-bold '>{title}</p>
                 <div className='flex md:flex-row md:justify-between xs:flex-col xs:justify-center xs:items-center xxs:flex-col xxs:justify-center xxs:items-center xs:space-y-2 xxs:space-y-2'>
-                    <div className='flex space-x-2'>
+                    <div className='flex space-x-2  w-auto'>
 
-                        <div className={`${status === "Private" ? 'bg-red-500 w-20 rounded-lg text-red-900 p-1' : status === "Public" ? 'bg-gray-500 w-20 rounded-lg text-gray-900 p-1' : ''}`}>
+                        <div className={`${status === "Private" ? 'bg-red-500 w-auto rounded-lg text-red-900 p-1' : status === "Public" ? 'bg-gray-500 w-auto rounded-lg text-gray-900 p-1' : ''}`}>
                             {status}
                         </div>
 
-                        <div className={`${capstone === "Capstone" ? 'bg-blue-500 w-20 rounded-lg text-blue-900 p-1' :
-                            capstone === "Personal Project" ? 'bg-green-500 w-28 rounded-lg text-green-900 p-1' :
-                                capstone === "Group Project" ? 'bg-yellow-500 w-28 rounded-lg text-yellow-900 p-1' : ''
+                        <div className={`${capstone === "Capstone" ? 'bg-blue-500 w-auto rounded-lg text-blue-900 p-1' :
+                            capstone === "Personal Project" ? 'bg-green-500 w-auto rounded-lg text-green-900 p-1' :
+                                capstone === "Group Project" ? 'bg-yellow-500 w-auto rounded-lg text-yellow-900 p-1' : ''
                             } text-nowrap`}>
                             {capstone}
                         </div>
 
-                        <div className={`${development === "Full Stack" ? " bg-violet-500 w-20 rounded-lg text-violet-900" : development === "Front End" ? "bg-yellow-500 w-20 rounded-lg text-yellow-900" : ""} text-nowrap flex items-center justify-center`}>
+                        <div className={`${development === "Full Stack" ? " bg-violet-500 w-auto rounded-lg text-violet-900" : development === "Front End" ? "bg-yellow-500 w-auto rounded-lg text-yellow-900" : ""} text-nowrap flex items-center justify-center`}>
                             {development}
                         </div>
 
